0cafb096a7c4f0e57f177111bba4156e32538266
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-03-21 10:30:19 +0000

    Update Gradle and plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.github.rodm.teamcity.DockerTeamCityEnvironment
 plugins {
     id ("io.github.rodm.teamcity-server") version "1.5"
     id ("io.github.rodm.teamcity-environments") version "1.5"
-    id ("org.sonarqube") version "3.4.0.2513"
+    id ("org.sonarqube") version "4.0.0.2929"
 }
 
 group = "com.github.rodm"
