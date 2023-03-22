8ae206e2819bc029a3ce01327d0512172419ebff
Rod MacKenzie <rod.n.mackenzie@gmail.com>
2023-03-22 09:49:37 +0000

    Update version for further development

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.github.rodm"
-version = "1.0.2"
+version = "1.0-SNAPSHOT"
 
 extra["teamcityVersion"] = project.findProperty("teamcity.api.version") as String? ?: "2018.1"
 extra["downloadsDir"] = project.findProperty("downloads.dir") as String? ?: "$rootDir/downloads"
